c1384587ee35305af167cdc56c4e13721aaff1f6
Subhasis Das <subhasisdas125@gmail.com>
2020-05-10 21:28:17 +0530

    added auth context and toggler

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ThemeContextProvider from "./contexts/ThemeContext";
+import AuthContextProvider from "./contexts/AuthContext";
 import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 import ThemeToggler from "./components/ThemeToggler";
@@ -8,9 +9,11 @@ function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggler />
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggler />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
